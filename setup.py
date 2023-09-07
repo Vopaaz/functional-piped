786cd36c207f1407de949423d9beb53c104bf193
Vopaaz <liyifan945@gmail.com>
2023-09-06 20:58:37 -0700

    Remove requirements.txt

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@ from setuptools import setup, find_packages
 with open(r"README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-with open(r"requirements.txt", "r", encoding="utf-8") as f:
-    install_requires = f.read().strip().split("\n")
-
 setup(
     name="dot-map",
     version="0.0.1",
@@ -16,7 +13,6 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
-    install_requires=install_requires,
     python_requires=">=3.8",
     classifiers=[
         "Programming Language :: Python :: 3.8",
